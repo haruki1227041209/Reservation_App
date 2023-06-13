32f880f542f61f17e57a5350ae6bd0612b248d99
haruki1227041209 <haruki122704@gmail.com>
2023-06-14 07:18:55 +0900

    アカウント登録後のセションを直した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to show_profile_path(@user)
     else
       render 'new'
@@ -17,15 +18,15 @@ class UsersController < ApplicationController
   end
 
   def show_account
-    @user = current_user
+    @user = User.find(params[:id])
   end
   
   def show
-    redirect_to show_profile_path
+    show_profile_path(current_user)
   end
 
   def show_profile
-    @user = current_user
+    @user = User.find(params[:id])
   end
   
   def edit_profile
